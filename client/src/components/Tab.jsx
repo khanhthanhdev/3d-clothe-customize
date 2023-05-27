4326540568f01c6ac5a56c3a79d458c9fe1f4018
khanhthanhdev <thanhkt27507@gmail.com>
2023-05-27 08:35:59 +0700

    add server, connect to openai, generate image from text

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const Tab = () => {
+import {useSnapshot} from 'valtio';
+
+import state from '../store';
+
+const Tab = ({tab, isFillterTab, isActiveTab, handleClick}) => {
+  const snap = useSnapshot(state)
+
+  const activeStyles = isFillterTab && isActiveTab 
+  ? {background: snap.color, opacity: 0.5}
+  : {background: "transparent", opacity:1}
   return (
-    <div>Tab</div>
+    <div key= {tab.name}
+     className={`tab-btn ${isFillterTab ? 'rounded-full glassmorhism': 'round-4'}`}   
+      onClick={handleClick}
+      stlye={activeStyles}
+    >
+      <img 
+        src = {tab.icon}
+        alt = {tab.name}
+        className = {`${isFillterTab ? 'w-2/3 h-2/3': 'w-11/12 h-11/12 object-contain'}`}
+      />
+    </div>
   )
 }
 
